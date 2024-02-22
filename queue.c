a1e7797804040e4dd4b955cd52a6bfd6e7cfa3cc
Rafael Takeguma <rafael08.0807@gmail.com>
2024-02-22 10:14:37 -0300

    implement isEmpty() and isFull()

--- queue.c
+++ queue.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #define SIZE 10
 
-int queue[SIZE];
-int Rear, Front = -1; 
+void* queue[SIZE];
+int Rear, Front = -1;
+
+int isEmpty() {
+    return (Front == -1); 
+}
+
+int isFull() {
+    return (Rear == SIZE -1);
+}
